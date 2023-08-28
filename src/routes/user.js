821c5c55caea433e0da59cddac3f5db915a8f192
Latifa Hussaini <latifa@Latifas-MBP.realtek>
2023-08-28 23:20:50 +0800

    fix typo

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const { authenticate, authorize } = require('../middleware/auth');
 const UserController = require('../controllers/UserController');
 
 router.post('/register', UserController.registerUser);
-router.post(';login', authenticate, authorize(['user', 'admin']), UserController.loginUser);
+router.post('/login', authenticate, authorize(['user', 'admin']), UserController.loginUser);
 router.post('/logout', authenticate, UserController.logOutUser);
 router.patch('/me', authenticate, UserController.updateUser);
 router.delete('/me', authenticate, UserController.deleteUser);
